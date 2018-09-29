4efa88b860cc502cf4f1dac16310c8bb62835487
jesubu <fjdiazr@gmail.com>
2018-09-29 10:08:53 +0200

    jesubu-->Middleware II

---
+++ Middleware/TimerMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apibeers.Middleware
+{
+    public class TimerMiddleware
+    {
+         private readonly RequestDelegate _next;
+        public TimerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext ctx)
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            await _next.Invoke(ctx);
+            watch.Stop();
+            var ms = watch.ElapsedMilliseconds;
+            Debug.WriteLine($"Took : {ms}");
+        }
+    }
+}
---
+++ Middleware/XMethodOverrideMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apibeers.Middleware
+{
+    public class XMethodOverrideMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public XMethodOverrideMiddleware(RequestDelegate next) => _next = next;
+
+
+        public async Task Invoke(HttpContext ctx)
+        {
+            //metodo asincrono
+            //Analizar la petición y modificar la petición si cabe
+            var request = ctx.Request;
+            if (request.Headers.ContainsKey("X-HTTP-Method-Override"))
+            {
+                var header = request.Headers["X-HTTP-Method-Override"];
+                request.Method = header;
+             }
+
+            //Generamos una resupuesta si queremos
+            await _next.Invoke(ctx);
+            //Analizamos la respuesta y modidicamos si cabe
+            //y cedemos el control al siguiente control
+
+            int i = 0;
+
+        }
+        
+    }
+}
